2f95ebbd3d2ef6ee088fb510cf6bbe6d1e234563
eduardoarsocca <eduardo_socca@yahoo.com.br>
2026-03-25 10:43:54 -0300

    testing regex alterations in procedures_maps.py

--- src/integracao/mappings/procedures_maps.py
+++ src/integracao/mappings/procedures_maps.py
@@ -165,7 +165,7 @@ V1_POLOTRIAL_PROCEDURES_MAP: List[Dict[str, Optional[str]]] = [
     },
      {
         #27
-        "procedure_name": r"Avaliação clínica subjetiva realizada pelo pesquisador \(escala IGA para pigmentação\)",
+        "procedure_name": r"Avaliação clínica subjetiva realizada pelo pesquisador\s+\(escala IGA para\s+pigmentação\)",
         "redcap_check_field": "hurhi_q2",
         "redcap_date_field": "hurhi_dt"
     },
@@ -558,7 +558,7 @@ V3_PROCEDURES_MAP: List[Dict[str, Optional[str]]] = [
     },
      {
         #27
-        "procedure_name": r"Avaliação clínica subjetiva realizada pelo pesquisador \(escala IGA para pigmentação\)",
+        "procedure_name": r"Avaliação clínica subjetiva realizada pelo pesquisador\s+\(escala IGA para\s+pigmentação\)",
         "redcap_check_field": "hurhi_q2",
         "redcap_date_field": "hurhi_dt"
     },
